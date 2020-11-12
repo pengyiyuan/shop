2394c19b3bf798a322b03fffb80cb92d80d17b3e
pengyiyuan <18666644561@163.com>
2020-11-12 21:24:47 +0800

    添加swagger插件

--- src/main/java/com/peng/shop/business/temp/web/TempController.java
+++ src/main/java/com/peng/shop/business/temp/web/TempController.java
@@ -8,9 +8,14 @@ import com.peng.shop.business.temp.dao.TempEntityMapper;
 import com.peng.shop.business.temp.entity.TempEntity;
 import com.peng.shop.business.temp.entity.TempEntityExample;
 import com.peng.shop.business.temp.model.SwiperVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -18,20 +23,26 @@ import java.util.List;
 
 @RestController
 @RequestMapping("temp")
+@Api(tags = "临时")
 public class TempController {
 
     @Autowired
     TempEntityMapper mapper;
 
     @GetMapping("test")
+    @ApiOperation(value = "测试")
     public ReturnJson test() {
-        PageHelper.startPage(3,2);
+        PageHelper.startPage(3, 2);
         List<TempEntity> tempEntities = mapper.selectByExample(new TempEntityExample());
         return ReturnJson.success(new PageInfo<TempEntity>(tempEntities));
     }
 
     @GetMapping("index/getSwiperData")
-    public ReturnJson getSwiperData() {
+    @ApiOperation(value = "获取轮播图")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "date", value = "时间")
+        })
+    public ReturnJson getSwiperData(@RequestParam(required = false) String date) {
         List<SwiperVO> swiperVOList = new ArrayList<>();
         swiperVOList.add(new SwiperVO(Constant.BaseConfig.SERVER_IP + "/shop/cb6410526aca76058b3cc83ebcc38fd9.jpg", 1));
         swiperVOList.add(new SwiperVO(Constant.BaseConfig.SERVER_IP + "/shop/0ed363a7a4cb909b1c3c84f792b5d01f.png", 2));
