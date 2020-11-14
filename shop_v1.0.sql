91a7ede1bccd0d4729688a96c88c94f9fe8cc781
pengyiyuan <18666644561@163.com>
2020-11-14 13:26:02 +0800

    sql增加商品分类

--- shop_v1.0.sql
+++ shop_v1.0.sql
@@ -9,5 +9,7 @@ CREATE TABLE IF NOT EXISTS `goods` (
   `goods_image` varchar(100) DEFAULT NULL COMMENT '商品大图',
   `goods_price` varchar(50) NOT NULL COMMENT '商品价格',
   `goods_num` bigint(20) NOT NULL COMMENT '商品数量',
+  `fist_category` tinyint COMMENT '一级分类',
+  `second_category` tinyint COMMENT '二级分类',
   PRIMARY KEY (`goods_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品表';
